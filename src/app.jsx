71dde06db3a585e0d3eb815816cb44fa91746746
sehoon <gsh723@naver.com>
2021-05-27 00:21:35 +0900

    [cardMaker - login] 1. login.jsx파일과 그에 맞는 login.module.css 파일 생성 2. login창의 html, css 구현
    * react상에서 img 태그가 일반 html에서와 달라서 애먹었다. 집에 와서 찬물에 샤워하고 가뿐한 마음으로 해서 해결!
    (On the React programming, using a <img> tag is differenct from using <img> tag in normal HTML. But after cold shower at home, I could solved that problem lightly)

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+import Login from './components/login/login';
+
+function App() {  
+  return (    
+    <>
+      <Login/>   
+     </>
+  );
+}
+
+export default App;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './login.module.css';
+import logo from '../../images/logo.png';
+
+const Login = (props) =>{
+    return(
+        <div className={styles.box}>
+            <div className={styles.container}>
+                <header className={styles.header}>
+                    <img className={styles.logo} src={logo} alt="logo" />             
+                    <h1 className = {styles.title}>Business Card Maker</h1>
+                </header>
+
+                <main className = {styles.main}>
+                    <h1 className = {styles.semiTitle}>Login</h1>
+                    <button className = {styles.loginGoogle}>Google</button>
+                    <button className = {styles.loginGitbub}>Github</button>
+                </main>
+
+                <footer className = {styles.footer}>
+                    <p className = {styles.footerPara}>Code your Dream</p>
+                </footer>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
